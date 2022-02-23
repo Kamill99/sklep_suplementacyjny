6e9b4a1c0ef4a89021e625d65f228e2c7223f1e9
Kamill99 <kamillfcb99@gmail.com>
2022-02-23 11:00:33 +0100

    Dodanie bootstrap

--- sklep/settings.py
+++ sklep/settings.py
@@ -131,13 +131,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "root")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static')
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'Main/static'),
+    os.path.join(BASE_DIR, 'Main/boot'),
+)
 
 
 # Default primary key field type
